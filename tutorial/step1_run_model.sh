a69a299e5f5fbd7bf2685e22ed1cd9799a3687e9
GlancerZ <93135125+GlancerZ@users.noreply.github.com>
2024-11-19 14:43:22 -0500

    Update step1_run_model.sh

--- tutorial/step1_run_model.sh
+++ tutorial/step1_run_model.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python ../DOLPHIN/model/smart_seq/run_model.py --p ../DeepExonas/model/smart_seq/hyperparameter_smart_seq.pkl
+python ../DOLPHIN/model/smart_seq/run_model.py --p ../DOLPHIN/model/smart_seq/hyperparameter_smart_seq.pkl
